c92a2700d834c740c8f3b040f9e3573e4672e2b7
Tatiana Moraru <tatianamoraru1998@gmail.com>
2020-07-17 14:09:01 -0500

    Update main.tf

--- deployments/terraform/main.tf
+++ deployments/terraform/main.tf
@@ -1,7 +1,6 @@
 provider "aws" {}
-
 resource "aws_instance" "jenkins" {
-  ami           = "ami-03d0faf9012d36090"
+  ami           = " ami-08f3d892de259504d"
   instance_type = "t2.micro"
   tags = {
     Name = "Jenkins"
