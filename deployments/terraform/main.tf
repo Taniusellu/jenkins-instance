65ee43db2d5dba26d46949ff56efb541e18697cf
Tatiana Moraru <tatianamoraru1998@gmail.com>
2020-07-25 09:18:21 -0500

    Update main.tf

--- deployments/terraform/main.tf
+++ deployments/terraform/main.tf
@@ -6,3 +6,14 @@ resource "aws_instance" "jenkins" {
     Name = "Jenkins"
   }
 }
+
+resource "aws_instance" "jenkins" {
+  ami_id           = "${var.ami_id}"
+  instance_type = "t2.micro"
+  # key_name      = "${aws_key_pair.jenkins_key.key_name}"
+  tags = {
+    Name = "${var.Name}"
+  }
+}
+variable "ami_id" {}
+variable "Name" {}
